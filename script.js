1e7147bb65622b2c7e4a1f71771ab778a3731d73
Mari-Pol <polak.mp@email.cz>
2023-01-05 22:21:08 +0100

    fixed spacing, start local storage

--- script.js
+++ script.js
@@ -1,37 +1,52 @@
-onload="handleAddTask()";
+// window.onload = loadTasks;
+
 const deleteBtns = document.getElementsByClassName("delete");
-  for(let i = 0; i < deleteBtns.length; i++) {
+for(let i = 0; i < deleteBtns.length; i++) {
     const deleteBtn = deleteBtns[i];
     deleteBtn.addEventListener("click", function(ev){
-      const liElement = ev.target.parentElement;
-      liElement.parentElement.removeChild(liElement);
+        const liElement = ev.target.parentElement;
+        liElement.parentElement.removeChild(liElement);
     });
   }
   
-    var list = document.getElementById("myList");
-    function handleAddTask(){
-        var value = document.getElementById("ToDoTask").value;
-        if (!value) {
-            alert("Please fill out toDo task");
-            return;
-        }
-        var li = document.createElement("LI");
-      
-        var deleteSpan = document.createElement('span')
-        deleteSpan.innerHTML = "x";
-        deleteSpan.classList.add("delete");
-        deleteSpan.addEventListener("click", function(ev){
-          const liElement = ev.target.parentElement;
-          liElement.parentElement.removeChild(liElement);
-        });
-        var textNode = document.createTextNode(value);
-      
-        li.appendChild(deleteSpan);
-        li.appendChild(textNode);
-        list.appendChild(li);
+var list = document.getElementById("myList");
+
+/* function loadTasks() {
+        // check if localStorage has any tasks
+        // if not then return
+        if (localStorage.getItem("myList") == null) return;
+  
+        // Get the tasks from localStorage and convert it to an array
+        let tasks = Array.from(JSON.parse(localStorage.getItem("myList")));
+
+
+      }
+*/
+function handleAddTask(){
+    var value = document.getElementById("ToDoTask").value;
+    if (!value) {
+        alert("Please fill out toDo task");
+        return;
     }
-    list.addEventListener("click", function(ev){
-        if (ev.target.tagName === "LI") {
-            ev.target.classList.toggle("checked");
-        }
-    }, false);
+    var li = document.createElement("LI");
+
+
+      
+    var deleteSpan = document.createElement('span')
+    deleteSpan.innerHTML = "x";
+    deleteSpan.classList.add("delete");
+    deleteSpan.addEventListener("click", function(ev){
+        const liElement = ev.target.parentElement;
+        liElement.parentElement.removeChild(liElement);
+    });
+    var textNode = document.createTextNode(value);    
+    li.appendChild(deleteSpan);
+    li.appendChild(textNode);
+    list.appendChild(li);
+}
+
+list.addEventListener("click", function(ev){
+    if (ev.target.tagName === "LI") {
+        ev.target.classList.toggle("checked");
+    }
+}, false);
