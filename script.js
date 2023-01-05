0283fe11fa5150e19fe8471528e5a813c2b75fca
Mari-Pol <polak.mp@email.cz>
2023-01-05 21:12:54 +0100

    Fixed spacing and if condition

--- script.js
+++ script.js
@@ -1,39 +1,37 @@
 onload="handleAddTask()";
-   const deleteBtns = document.getElementsByClassName("delete");
-        for(let i = 0; i < deleteBtns.length; i++) {
-            const deleteBtn = deleteBtns[i];
-            deleteBtn.addEventListener("click", function(ev){
-                    const liElement = ev.target.parentElement;
-                    liElement.parentElement.removeChild(liElement);
-                    });
-            }
-                            
+const deleteBtns = document.getElementsByClassName("delete");
+  for(let i = 0; i < deleteBtns.length; i++) {
+    const deleteBtn = deleteBtns[i];
+    deleteBtn.addEventListener("click", function(ev){
+      const liElement = ev.target.parentElement;
+      liElement.parentElement.removeChild(liElement);
+    });
+  }
+  
     var list = document.getElementById("myList");
     function handleAddTask(){
+        var value = document.getElementById("ToDoTask").value;
         if (!value) {
             alert("Please fill out toDo task");
             return;
         }
-    var value = document.getElementById("ToDoTask").value;
-    var li = document.createElement("LI");
- 
-    var deleteSpan = document.createElement('span')
-    deleteSpan.innerHTML = "x";
-    deleteSpan.classList.add("delete");
-    deleteSpan.addEventListener("click", function(ev){
-        const liElement = ev.target.parentElement;
-        liElement.parentElement.removeChild(liElement);
+        var li = document.createElement("LI");
+      
+        var deleteSpan = document.createElement('span')
+        deleteSpan.innerHTML = "x";
+        deleteSpan.classList.add("delete");
+        deleteSpan.addEventListener("click", function(ev){
+          const liElement = ev.target.parentElement;
+          liElement.parentElement.removeChild(liElement);
         });
-
         var textNode = document.createTextNode(value);
       
         li.appendChild(deleteSpan);
         li.appendChild(textNode);
         list.appendChild(li);
-        }
-
+    }
     list.addEventListener("click", function(ev){
         if (ev.target.tagName === "LI") {
             ev.target.classList.toggle("checked");
-            }
-        }, false);
+        }
+    }, false);
